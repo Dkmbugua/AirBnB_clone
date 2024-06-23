eb3cffc7872f8feb41995d1b75265e9ca04e68cd
Dkmbugua <dkmbugua70@gmail.com>
2024-06-23 15:39:30 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -13,21 +13,24 @@ class BaseModel:
 
     def __str__(self):
         """provides a readable string representation of the instance"""
-        return f'{self.__class__.__name__}({self.id}, {self.__dict__})'
+        return f'{self.__class__.__name__}({self.id}), {self.__dict__}'
 
 
     def save(self):
         """it updates the update_at attribute with the current time"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        try:
+            models.storage.save(self)
+        except Exception as e:
+            print(f"An error occured while saving: {e}")
 
 
     def to_dict(self):
-        """it returns a dictionary whcich conatin all key values"""
+        """it returns a dictionary containing all key values pairs."""
         dk_dict = self.__dict__.copy()
         dk_dict['updated_at'] = self.updated_at.isoformat()
         dk_dict['__class__'] = self.__class__.__name__
-        dk_dict['created_at'] =self.create_at.isoformat()
+        dk_dict['created_at'] = self.created_at.isoformat()
 
         return dk_dict
 
